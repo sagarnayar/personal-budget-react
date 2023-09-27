3666b6ff07a6faa989f52a30119ee69540a41fa4
sagar <sagarnayar@gmail.com>
2023-09-27 17:22:10 -0400

    Pushed code

---
+++ src/Footer/Footer.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="bottom">
+
+
+    <div className="center" aria-label="COPYRIGHT INFORMATION">
+
+      All rights reserved &copy; Sagar Nayar
+
+
+      <div className="h-card">
+
+        <p className="p-name">Sagar</p>
+        &nbsp;
+
+
+        <a className="u-email" href="mailto:snayar@uncc.edu">Email me</a>&nbsp;
+
+        <p className="p-locality">NC</p>
+        ,&nbsp;
+        <p className="p-country-name">USA</p>
+      </div>
+    </div>
+  </footer>
+  );
+}
+
+export default Footer;
